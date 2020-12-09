1aff4d345b1c34381cda4a0b2a9d01acd6b4f258
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2020-12-09 11:41:12 -0300

    feat: add select publisher on register book screen

--- src/controllers/publisherController.js
+++ src/controllers/publisherController.js
@@ -1,66 +1,68 @@
-import 'regenerator-runtime'
-import { pool } from '../config/db'
+import 'regenerator-runtime';
+import { pool } from '../config/db';
 
 export async function registerPublisher(req, res) {
-    try {
-        const { nome } = req.body
-        const editora = await pool.query(
-            'INSERT INTO editora (nome) VALUES($1) RETURNING *;', [
-                nome
-            ])
-        res.json(editora.rows[0])
-    } catch (err) {
-        console.log(err.message)
-    }
+  try {
+    const { nome } = req.body;
+    const editora = await pool.query(
+      'INSERT INTO editora (nome) VALUES($1) RETURNING *;',
+      [nome]
+    );
+    res.json(editora.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 export async function getPublisher(req, res) {
-    try {
-        const { codigo_editora } = req.params
-        const editora = await pool.query(
-            'SELECT (nome) FROM editora WHERE editora.codigo_editora = $1;', [
-            codigo_editora
-        ])
-        res.json(editora.rows[0]);
-    } catch (err) {
-        console.log(err.message);
-    }
+  try {
+    const { codigo_editora } = req.params;
+    const editora = await pool.query(
+      'SELECT (nome) FROM editora WHERE editora.codigo_editora = $1;',
+      [codigo_editora]
+    );
+    res.json(editora.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 export async function getPublishers(req, res) {
-    try {
-        const editoras = await pool.query(
-            'SELECT (codigo_editora, nome) FROM editora;'
-        )
-        res.json(editoras.rows[0]);
-    } catch (err) {
-        console.log(err.message);
-    }
+  try {
+    const editoras = await pool.query(
+      'SELECT codigo_editora, nome FROM editora;'
+    );
+    const editoras_result = editoras.rows;
+    return res.render('views/registerBook.html', { editoras_result });
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 export async function updatePublisher(req, res) {
-    try {
-        const { codigo_editora } = req.params
-        const { nome } = req.body
-        const editora = await pool.query( 'UPDATE editora SET nome = $1 WHERE editora.codigo_editora = $2', [
-                nome, codigo_editora
-            ])
+  try {
+    const { codigo_editora } = req.params;
+    const { nome } = req.body;
+    const editora = await pool.query(
+      'UPDATE editora SET nome = $1 WHERE editora.codigo_editora = $2',
+      [nome, codigo_editora]
+    );
 
-        res.json(editora.rows[0])
-    } catch (err) {
-        console.log(err.message)
-    }
+    res.json(editora.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 export async function deletePublisher(req, res) {
-    try {
-        const { codigo_editora } = req.params
-        const editora = await pool.query(
-            'DELETE FROM editora WHERE editora.codigo_editora = $1;', [
-            codigo_editora 
-        ])
-        res.json(editora.rows[0]);
-    } catch (err) {
-        console.log(err.message)
-    }
+  try {
+    const { codigo_editora } = req.params;
+    const editora = await pool.query(
+      'DELETE FROM editora WHERE editora.codigo_editora = $1;',
+      [codigo_editora]
+    );
+    res.json(editora.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
--- src/routes/publisherRouter.js
+++ src/routes/publisherRouter.js
@@ -1,13 +1,13 @@
-import 'regenerator-runtime'
-import express from 'express'
-import * as publisherController from '../controllers/publisherController'
+import 'regenerator-runtime';
+import express from 'express';
+import * as publisherController from '../controllers/publisherController';
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/register', publisherController.registerPublisher)
-router.get('/:codigo_editora', publisherController.getPublisher)
-router.get('/', publisherController.getPublishers)
-router.put('/:codigo_editora', publisherController.updatePublisher)
-router.delete('/:codigo_editora', publisherController.deletePublisher)
+router.post('/register', publisherController.registerPublisher);
+router.get('/:codigo_editora', publisherController.getPublisher);
+router.get('/', publisherController.getPublishers);
+router.put('/:codigo_editora', publisherController.updatePublisher);
+router.delete('/:codigo_editora', publisherController.deletePublisher);
 
-export default router
+export default router;
