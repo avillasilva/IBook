58fcbe8c225e48aafbaf05c521092d190de17ae8
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2020-12-10 13:50:19 -0300

    feat: connecting book register to back end

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -1,13 +1,13 @@
-import 'regenerator-runtime'
-import express from 'express'
-import * as bookController from '../controllers/bookController'
+import 'regenerator-runtime';
+import express from 'express';
+import * as bookController from '../controllers/bookController';
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/register', bookController.registerBook)
-router.get('/:codigo_livro', bookController.getBook)
-router.get('/', bookController.getBooks)
-router.put('/:codigo_livro', bookController.updateBook)
-router.delete('/:codigo_livro', bookController.deleteBook)
+router.post('/register', bookController.registerBook);
+router.get('/:codigo_livro', bookController.getBook);
+router.get('/', bookController.getBooks);
+router.put('/:codigo_livro', bookController.updateBook);
+router.delete('/:codigo_livro', bookController.deleteBook);
 
-export default router
+export default router;
